7319cdd6f125269081399f658a762202ab3ade8f
knu9910 <knu991@naver.com>
2020-05-21 23:18:31 +0900

    Sequelize-cli 기본설정 완료 및 users, musics 테이블 작성완료
     - sequelize-cli init 완료 기본파일 생성
     - users 테이블 생성
     - musics 테이블 생성
     - eslint 룰 추가
     - users, musics 테이블 관계 생성 완료

---
+++ migrations/20200520122424-create-musics.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('musics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      thumbnail: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      playtime: {
+        type: Sequelize.INTEGER
+      },
+      video_url: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('musics');
+  }
+};
