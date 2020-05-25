151b70a8a8309647f956bf884841ceaa442bcd7b
socratone <gim2origin@gmail.com>
2020-05-25 13:38:25 +0900

    Remove signout route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,6 @@ const { getMusiclist, postMusiclist, deleteMusiclist } = require('./musiclist');
 
 module.exports = {
   signin : require('./signin'),
-  signout: require('./signout'),
   signup: require('./signup'),
   getMusiclist,
   postMusiclist,
