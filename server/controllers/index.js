c41627e5cb81c4b71c3b0823d83e6a54efe64adb
socratone <gim2origin@gmail.com>
2020-05-25 13:33:47 +0900

    Add routes
     - deletedMusiclist와 writePlaytime 라우트를 추가했습니다.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,11 @@
-const {getMusiclist, postMusiclist} = require('./musiclist');
-
+const { getMusiclist, postMusiclist, deleteMusiclist } = require('./musiclist');
 
 module.exports = {
   signin : require('./signin'),
   signout: require('./signout'),
   signup: require('./signup'),
   getMusiclist,
-  postMusiclist
+  postMusiclist,
+  deleteMusiclist,
+  writePlaytime : require('./writePlaytime')
 };
